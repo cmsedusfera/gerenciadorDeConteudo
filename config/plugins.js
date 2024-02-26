653d99e055fae652fe535b97191db384867bf842
SANCLOCAL\jrsilva <jrsilva@santillana.com>
2024-02-26 10:39:09 -0300

    config projeto

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
       enabled: true,
       config: {
         deployHook:
-          "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+          "https://api.vercel.com/v1/integrations/deploy/prj_8YoLH9OohSGyofplaZwukwBj43NX/fO6PlMK4UG",
         apiToken: "weGHNOGnkFoXHLuniPQIhHZi",
-        appFilter: "jeffersons-projects-52b3a927",
+        appFilter: "gerenciador-de-conteudo",
         teamFilter: "team_N2pI6pbMKUfk0l11Zfy0kA6T",
         roles: ["strapi-super-admin"],
       },
