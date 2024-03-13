726651a4d61fbcb6717573f05aaff1bfd68e1ec3
SANCLOCAL\jrsilva <jrsilva@santillana.com>
2024-03-13 16:06:52 -0300

    removendo vercel

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,1 @@
-module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
-      config: {
-        deployHook:
-          "https://api.vercel.com/v1/integrations/deploy/prj_8YoLH9OohSGyofplaZwukwBj43NX/fO6PlMK4UG",
-        apiToken: "weGHNOGnkFoXHLuniPQIhHZi",
-        appFilter: "gerenciador-de-conteudo",
-        teamFilter: "team_N2pI6pbMKUfk0l11Zfy0kA6T",
-        roles: ["strapi-super-admin"],
-      },
-    },
-  });
+module.exports = ({ env }) => ({});
